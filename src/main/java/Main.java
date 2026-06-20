16829dcfdf774aa827e5653f1deaefe8ca8c9b0d
Aditeey Singh <aditeeysinghjadon@gmail.com>
2026-06-20 15:17:20 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,6 +18,22 @@ public class Main {
                 continue;
             }
 
+            if (input.startsWith("type ")) {
+                String command = input.substring(5);
+
+                if (command.equals("echo")) {
+                    System.out.println("echo is a shell builtin");
+                } else if (command.equals("exit")) {
+                    System.out.println("exit is a shell builtin");
+                } else if (command.equals("type")) {
+                    System.out.println("type is a shell builtin");
+                } else {
+                    System.out.println(command + ": not found");
+                }
+
+                continue;
+            }
+
             System.out.println(input + ": command not found");
         }
     }
